633412864ceb607f50b585bfdcc9b5a612bbb800
kutniti-watch <145538297+kutniti-watch@users.noreply.github.com>
2023-09-20 12:50:07 +0530

    package file uploaded

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+      custom: ['Montserrat', 'sans-serif'],
+      boxShadow: {
+        'all-sides': '0 0 10px rgba(0, 0, 0, 0.2)', // Customize the shadow here
+      },
+    },
+    
+},
+  },
+  plugins: [],
+};
